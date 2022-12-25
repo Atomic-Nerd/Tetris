49cee4bf0ad8d3f025fe0fd702e828d9007cb549
Atomic-Nerd <jonathanashworth101@gmail.com>
2022-12-25 01:24:44 +0000

    Adding scores

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 screen = pygame.display.set_mode((750, 600))
 pygame.display.set_caption("Tetris")
 
+font = pygame.font.SysFont('consolas', 30)
+
 GRID_SIZE = 25
 GRAY = (128,128,128)
 BOX_COLOURS = [None,(255,0,0),(0,32,255),(0,192,0),(255,255,0)]
@@ -13,7 +15,26 @@ BOX_COLOURS = [None,(255,0,0),(0,32,255),(0,192,0),(255,255,0)]
 
 grid = [[0 for i in range(10)] for i in range(20)]
 
+class player:
+    def __init__(self):
+        self.grid_x = 0
+        self.grid_y = 0
+        self.score = 0
+        self.lines = 0
+
 def draw():
+
+    screen.fill((0,0,0))
+
+    scoreText = font.render(f"Score: {str(user.score)}", True, (255,255,0))
+    linesText = font.render(f"Lines: {str(user.lines)}", True, (255,255,0))
+
+    screen.blit(scoreText, (500, 100))
+    screen.blit(linesText, (500, 130))
+
+    nextText = font.render("Next:", True, (255,255,0))
+
+    screen.blit(nextText, (500, 250))
 
     for i in range(10):
         for j in range(20):
@@ -22,7 +43,13 @@ def draw():
                 colour_code = grid[j][i]
                 pygame.draw.rect(screen, BOX_COLOURS[colour_code], (1+100 + i * GRID_SIZE, 1+50 + j * GRID_SIZE, GRID_SIZE-2, GRID_SIZE-2))
 
+    for i in range(4):
+        for j in range(4):
+            pygame.draw.rect(screen, GRAY, (490+i*GRID_SIZE, 300+j*GRID_SIZE, GRID_SIZE, GRID_SIZE),1)
+
     pygame.display.update()
+
+user = player()
 
 while True:
 
